1735351b13a2dc471cf92a7ded94cfa5975082c0
abdalrahmanf2 <abdalrahman.vim@gmail.com>
2025-04-01 19:11:11 +0200

    woew

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -93,6 +93,23 @@ return {
 			ensure_installed = {
 				"stylua",
 				"prettier",
+				"eslint_d",
+			},
+		})
+
+		vim.diagnostic.config({
+			virtual_text = true,
+			float = true,
+			underline = true,
+			update_in_insert = false,
+			severity_sort = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "✘",
+					[vim.diagnostic.severity.WARN] = "▲",
+					[vim.diagnostic.severity.HINT] = "⚑",
+					[vim.diagnostic.severity.INFO] = "»",
+				},
 			},
 		})
 
